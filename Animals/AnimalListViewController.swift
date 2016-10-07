f04c3fa118824862dba629a72e3a4cccee7ce983
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-10-07 08:35:59 -0400

    Complete

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Animals
+//  animalList
 //
 //  Created by Michael Dippery on 7/17/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
@@ -9,4 +9,43 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    var animalList = [
+        "Dog",
+        "Cat",
+        "Mouse",
+        "Hamster",
+        "Bunny",
+        "Panda",
+        "Lion",
+        "Pig",
+        "Frog",
+        "Octopus",
+        
+        ]
+    
+    //determine number of sections in tableView
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //determine number of Rows in Section, return the number of items in the item list
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalList.count
+    }
+    
+    //determine the cell text at a given index path and row using a prototype cell identifier
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        cell.textLabel?.text = String(animalList[indexPath.row])
+        return cell
+    }
+    //prepare for the segue, by pointing to the right segue identifier, then cast the segue.destination as the viewController class you'll be using for the View.  Then set the destination variable value to the list contents at the selected indexPath.row.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "AnimalDetailView" { return }
+        if let dest = segue.destination as? AnimalViewController, let indexPath = tableView.indexPathForSelectedRow {
+            dest.animalName = animalList[indexPath.row]
+        }
+    }
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,63 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    var animalName: String?
+    var animalEmoji: String?
+    
+    
+    //Set the smaller text label to the English name of the animal represented in this view.
+    //Set the larger text label to the emoji corresponding to the animal. (You may want to create a helper method to map the English name of the animal to its emoji.)
+    //Set the nav bar title to the English name of the animal.
+    override func viewWillAppear(_ animated: Bool) {
+        nameToEmoji()
+        emojiLabel.text = animalEmoji
+        nameLabel.text = animalName
+    }
+    
+    func nameToEmoji(){
+        if let animal = animalName {
+            switch animal {
+            case "Dog":
+                animalEmoji = "🐶"
+            case "Cat":
+                animalEmoji = "🐱"
+            case "Mouse":
+                animalEmoji = "🐭"
+            case "Hamster":
+                animalEmoji = "🐹"
+            case "Bunny":
+                animalEmoji = "🐰"
+            case "Panda":
+                animalEmoji = "🐼"
+            case "Lion":
+                animalEmoji = "🦁"
+            case "Pig":
+                animalEmoji = "🐷"
+            case "Frog":
+                animalEmoji = "🐸"
+            case "Octopus":
+                animalEmoji = "🐙"
+            default:
+                animalEmoji = "❓"
+            }
+        }
+    }
 }
+
+
+
+
+    
+    
+
+    
+    
+    
+    
+
